ed5f4e979e5c2144a5d4ecfc0b5de3c5a2634daf
486ben <84605043+486ben@users.noreply.github.com>
2022-04-30 17:34:20 -0700

    Update menu.js

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -5,27 +5,16 @@ class Menu extends Phaser.Scene {
 
     preload() {
         // load music audio
-        this.load.image('menuBackground', './assets/menuBackground.png');
+        this.load.image('menuBackground', './assets/intro1.png');
         //audio for skeleton
         this.load.audio('skeleton_laugh', './assets/skeleton_laugh.mp3');
-
         //audio for background
         this.load.audio('bg_music', './assets/bg_music.wav');
-
         //jump sound
         this.load.audio('jump_sound', './assets/jump_sound.wav');
-
-        //background image
-       
-
-
-
-
     }
 
     create() {
-        
-        
         // menu text configuration
         let menuConfig = {
             fontFamily: 'Courier',
@@ -39,38 +28,45 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
-        
+
+        //menu text UI
+        let centerX = game.config.width / 2;
+        let centerY = game.config.height / 2;
+
         //add background music
         this.music = this.sound.add('bg_music', {mute: false, volume: 1.0, rate: 1, loop: true});
         this.music.play();
-
+        
         //show background
         this.add.tileSprite(0, 0, 1200, 700, 'menuBackground').setOrigin(0, 0);
 
         // show menu text
         menuConfig.color = '#000';
         menuConfig.backgroundColor = '#00FF00';
-        this.add.text(game.config.width/2, game.config.height/3 - borderUISize - borderPadding, 'Eternal Halloween', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Press SPACE to play', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY/3 - borderUISize - borderPadding, 'Eternal Halloween', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY/2 + 100, 'Press S to creator', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY/2 + 40, 'Press W to Rule', menuConfig).setOrigin(0.5);
+        this.add.text(centerX, centerY/2 - 40, 'Press SPACE to play', menuConfig).setOrigin(0.5);
 
         // define keys
-        //keyRIGHT = this.input.keyboard.addkey(Phaser.Input.KeyCodes.RIGHT);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-        keyRULE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        KeyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
         
     }
 
     update() {
+        //Space to start the game
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-            //this.sound.play("", { volume: 2.0 });
-            //this.sound.play('sfx_select');
-            this.music.stop();
             this.scene.start("playScene");
         }
-        if(Phaser.Input.Keyboard.JustDown(keyRULE)){
+        //W for rule page
+        if(Phaser.Input.Keyboard.JustDown(KeyW)){
             this.scene.start("ruleScene");
         }
-         
+        //S for rule page
+        if(Phaser.Input.Keyboard.JustDown(keyS)){
+            this.scene.start("creatorScene");
+        }
         } 
     }
