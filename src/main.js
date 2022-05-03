ac5855c7e9bc46bf9b19b7b4c6cc9d553249c929
wichapas <wpichetp@ucsc.edu>
2022-05-03 01:15:44 -0700

    main.js update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+//In main.js (or equivalent), include a header with collaborator names, game title, date completed, and your creative tilt justification (see below) (5)
+/*
+Wichapas Pichetpongsa
+Amanda Bahadori
+Yunhan Wei
+Zhibin Huang
+
+5/3/2022
+
+The game takes a more of interesting in terms of aesthetics rather than mechanics. Although the mechanic is simple, it's essentially what endless runner is about.
+What I liked about this game the most is how you almost have like a second stage once the timer hits 20 seconds. Scene is changed, and new obstacles appears. The obstacles are
+changed to fit the theme of the scene and this game follows a narrative with our theme in mind. The programming part that I enjoyed is managing the groups and changing the scene
+when 20 seconds has passed. With the addition of the skeleton, it creates a sense of danger and mirrors the movement of your character. We had a hard time coding this so
+we went beyond our abilities to try to get this game to work.
+
+This game has a great visual style and I'd say we accomplished with the theme we were going for. Halloween, creepy, and cute is essentially what makes the world of our game immersive.
+The music also adds another layer of spookiness in the main menu but once the game starts you can feel the intensity increasing with a different bgm playing. This game is the
+embodiment of cute spookiness!
+*/
+
 //global game settings
 
 
